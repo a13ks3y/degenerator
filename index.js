2000c2ed502afe9b02ab635d71c496ecf8d74d42
Oleksii Smirnov <o.smirnov@e-tender.ua>
2021-04-01 23:36:21 +0300

    add password strength checker

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const emailInputEl = document.getElementById('email');
 const passwordOutputEl = document.getElementById('password');
+const passwordFeedbackEl = document.getElementById('feedback');
 emailInputEl.addEventListener('change', () => {
     const email = emailInputEl.value;
     const password = email.replace('@', '_AT_').replace(/\./g, '-DOT-');
     passwordOutputEl.innerHTML = password;
+    const checkResult = zxcvbn(password);
+    const output = Object.keys(checkResult.crack_times_display).reduce((acc, c) => {
+       return acc + `${c}: <strong>${checkResult.crack_times_display[c]}</strong><br/>`;
+    }, 'Crack Time Estimation:<br/>\n');
+    passwordFeedbackEl.innerHTML = output;
 });
